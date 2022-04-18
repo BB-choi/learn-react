1e18953973b9e1f8cafc9b979d7dc1be57584fa4
BB-choi <choibobae0809@gmail.com>
2022-04-18 23:28:48 +0900

    8. Create

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -54,15 +54,42 @@ function Article({ title, body }) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title"></input>
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
 
-  const topics = [
+  const [topics, setTopics] = useState([
     { id: 1, title: "html", body: "html is..." },
     { id: 2, title: "css", body: "css is..." },
     { id: 3, title: "javascript", body: "javascript is..." },
-  ];
+  ]);
 
   let content = null;
   if (mode === "WELCOME") {
@@ -70,6 +97,17 @@ function App() {
   } else if (mode === "READ") {
     const [data] = topics.filter((el) => el.id === id);
     content = <Article title={data.title} body={data.body}></Article>;
+  } else if (mode === "CREATE") {
+    content = (
+      <Create
+        onCreate={(title, body) => {
+          setTopics([...topics, { id: nextId, title, body }]);
+          setMode("READ");
+          setId(nextId);
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
   }
 
   return (
@@ -88,6 +126,15 @@ function App() {
         }}
       ></Nav>
       {content}
+      <a
+        href="/create"
+        onClick={(e) => {
+          e.preventDefault();
+          setMode("CREATE");
+        }}
+      >
+        Create
+      </a>
     </div>
   );
 }
