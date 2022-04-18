20715bfeeee4ea2793e97467517e3b2763cc7dce
BB-choi <choibobae0809@gmail.com>
2022-04-18 09:30:33 +0900

    6. 이벤트

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,7 +5,15 @@ function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">{props.title}</a>
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
       </h1>
     </header>
   );
@@ -15,7 +23,16 @@ function Nav(props) {
   const lis = props.topics.map((topic) => {
     return (
       <li key={topic.id}>
-        <a href={"/read/" + topic.id}>{topic.title}</a>
+        <a
+          id={topic.id}
+          href={"/read/" + topic.id}
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode(e.target.id);
+          }}
+        >
+          {topic.title}
+        </a>
       </li>
     );
   });
@@ -44,8 +61,18 @@ function App() {
   ];
   return (
     <div className="App">
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header
+        title="WEB"
+        onChangeMode={() => {
+          alert("header");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}
+      ></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
